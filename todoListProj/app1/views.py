cc19245433be6d5d9df1440f14655fe3bfac0c4e
spancavil <soancavil@gmail.com>
2021-05-09 20:24:40 -0300

    Remove de app1

--- todoListProj/app1/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-# from .models import ListaQueHaceres
-
-# Create your views here.
-def index(request):
-    # todo_items = ListaQueHaceres.objects.order_by('id')
-    # context = {'todo_items' : todo_items}
-    return render(request,'app1/index.html')
